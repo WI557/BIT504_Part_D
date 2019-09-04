940702e350649e2c08df1ac95364162ac33021a6
WI557 <IainW557@outlook.com>
2019-09-04 19:59:02 +1200

    #3

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -5,6 +5,7 @@ public class PartD {
 		// TODO Auto-generated method stub
 System.out.println("Hi, my name is Iain");
 System.out.println("My favourite colour is Black");
+System.out.println("My favourite mode of transportation is car");
 	}
 
 }
